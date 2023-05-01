ced9e978f63fb39d6d12925d76021caf60f97c9a
DjDeveloperr <djsandhu2005@gmail.com>
2023-05-01 20:27:29 +0530

    init

---
+++ components/App.tsx
@@ -0,0 +1,25 @@
+import { Head } from "$fresh/runtime.ts";
+import { ComponentChildren } from "preact";
+
+export interface AppProps {
+  title: string;
+  description: string;
+  children?: ComponentChildren;
+}
+
+export function App(props: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>{props.title}</title>
+        <meta name="description" content={props.description} />
+        <meta name="og:title" content={props.title} />
+        <meta name="og:description" content={props.description} />
+        <link rel="stylesheet" href="/style.css" />
+      </Head>
+      <div class="app-container">
+        {props.children}
+      </div>
+    </>
+  );
+}
---
+++ utils/social.ts
@@ -0,0 +1,34 @@
+export const socials = {
+  github: {
+    label: "DjDeveloperr",
+    url: "https://github.com/DjDeveloperr",
+  },
+  twitter: {
+    label: "@DeveloperrDj",
+    url: "https://twitter.com/DeveloperrDj",
+  },
+  discord: {
+    label: "djdev#7001",
+    url: "https://discord.com/users/422957901716652033",
+  },
+  email: {
+    label: "dj@harmony.rocks",
+    url: "mailto:dj@harmony.rocks",
+  },
+  kofi: {
+    label: "@DjDeveloper",
+    url: "https://ko-fi.com/djdeveloper",
+  },
+  sponsor: {
+    label: "GitHub Sponsors",
+    url: "https://github.com/sponsors/DjDeveloperr",
+  },
+  instagram: {
+    label: "@djsandhu92",
+    url: "https://www.instagram.com/djsandhu92/",
+  },
+  snapchat: {
+    label: "@djsandhu92",
+    url: "https://www.snapchat.com/add/djsandhu92",
+  },
+} as const;
